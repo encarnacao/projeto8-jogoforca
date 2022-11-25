6a00b1e13e38dcf1594e65de8c3597e4052b0963
encarnacao <cencarnacaoq@gmail.com>
2022-11-24 22:50:39 -0300

    Melhorado a forma como cria os botões

--- src/components/Letras.js
+++ src/components/Letras.js
@@ -1,32 +1,13 @@
 export default function Letras(props) {
-    const letras = [
-        { letra: 'A', disabled: props.letras.letrasClicadas.includes('a'), jogada: () => jogada('a') },
-        { letra: 'B', disabled: props.letras.letrasClicadas.includes('b'), jogada: () => jogada('b') },
-        { letra: 'C', disabled: props.letras.letrasClicadas.includes('c'), jogada: () => jogada('c') },
-        { letra: 'D', disabled: props.letras.letrasClicadas.includes('d'), jogada: () => jogada('d') },
-        { letra: 'E', disabled: props.letras.letrasClicadas.includes('e'), jogada: () => jogada('e') },
-        { letra: 'F', disabled: props.letras.letrasClicadas.includes('f'), jogada: () => jogada('f') },
-        { letra: 'G', disabled: props.letras.letrasClicadas.includes('g'), jogada: () => jogada('g') },
-        { letra: 'H', disabled: props.letras.letrasClicadas.includes('h'), jogada: () => jogada('h') },
-        { letra: 'I', disabled: props.letras.letrasClicadas.includes('i'), jogada: () => jogada('i') },
-        { letra: 'J', disabled: props.letras.letrasClicadas.includes('j'), jogada: () => jogada('j') },
-        { letra: 'K', disabled: props.letras.letrasClicadas.includes('k'), jogada: () => jogada('k') },
-        { letra: 'L', disabled: props.letras.letrasClicadas.includes('l'), jogada: () => jogada('l') },
-        { letra: 'M', disabled: props.letras.letrasClicadas.includes('m'), jogada: () => jogada('m') },
-        { letra: 'N', disabled: props.letras.letrasClicadas.includes('n'), jogada: () => jogada('n') },
-        { letra: 'O', disabled: props.letras.letrasClicadas.includes('o'), jogada: () => jogada('o') },
-        { letra: 'P', disabled: props.letras.letrasClicadas.includes('p'), jogada: () => jogada('p') },
-        { letra: 'Q', disabled: props.letras.letrasClicadas.includes('q'), jogada: () => jogada('q') },
-        { letra: 'R', disabled: props.letras.letrasClicadas.includes('r'), jogada: () => jogada('r') },
-        { letra: 'S', disabled: props.letras.letrasClicadas.includes('s'), jogada: () => jogada('s') },
-        { letra: 'T', disabled: props.letras.letrasClicadas.includes('t'), jogada: () => jogada('t') },
-        { letra: 'U', disabled: props.letras.letrasClicadas.includes('u'), jogada: () => jogada('u') },
-        { letra: 'V', disabled: props.letras.letrasClicadas.includes('v'), jogada: () => jogada('v') },
-        { letra: 'W', disabled: props.letras.letrasClicadas.includes('w'), jogada: () => jogada('w') },
-        { letra: 'X', disabled: props.letras.letrasClicadas.includes('x'), jogada: () => jogada('x') },
-        { letra: 'Y', disabled: props.letras.letrasClicadas.includes('y'), jogada: () => jogada('y') },
-        { letra: 'Z', disabled: props.letras.letrasClicadas.includes('z'), jogada: () => jogada('z') },
-    ];
+    
+    const alfabeto = "abcdefghijklmnopqrstuvwxyz".split("");
+    const letras = alfabeto.map((l) => (
+        {
+            letra: l.toUpperCase(), 
+            disabled: props.letras.letrasClicadas.includes(l), 
+            jogada: ()=> jogada(l)
+        }
+        )); 
 
 
     function revelarLetras(letra, arrayPalavra, arrayPalavraEscondida) {
