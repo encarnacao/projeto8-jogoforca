045649534ddfe8e49f1ab91dce21b80182f213f1
encarnacao <cencarnacaoq@gmail.com>
2022-11-25 22:25:42 -0300

    Public e src reorganizadas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import Jogo from './components/Jogo.js';
 import Letras from './components/Letras.js';
 import Chute from './components/Chute.js';
 
+import erro0 from './assets/forca0.png';
+import erro1 from './assets/forca1.png';
+import erro2 from './assets/forca2.png';
+import erro3 from './assets/forca3.png';
+import erro4 from './assets/forca4.png';
+import erro5 from './assets/forca5.png';
+import erro6 from './assets/forca6.png';
+
+
 function App() {
   const [erros, setErros] = React.useState(0);
   const [palavra, setPalavra] = React.useState('');
@@ -16,8 +25,10 @@ function App() {
     return palavra.normalize("NFD").replace(/[^a-zA-Z\s]/g, "");
   }
 
+  const imagensErros = [erro0, erro1, erro2, erro3, erro4, erro5, erro6];
+
   const jogo = {
-    imagem: `./assets/images/forca${erros}.png`,
+    imagem: imagensErros[erros],
     palavraEscondida,
     palavra,
     classPalavra,
